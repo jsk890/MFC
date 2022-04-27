430e7948745fb9e7da7e4fb64a14a412dfc7142a
jsk890 <resqme89@naver.com>
2022-04-27 17:14:40 +0900

    chat App

---
+++ Chat/Chat.cpp
@@ -0,0 +1,48 @@
+﻿
+// Chat.cpp: 애플리케이션에 대한 클래스 동작을 정의합니다.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "Chat.h"
+#include "ChatDlg.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CChatApp
+
+BEGIN_MESSAGE_MAP(CChatApp, CWinApp)
+	ON_COMMAND(ID_HELP, &CWinApp::OnHelp)
+END_MESSAGE_MAP()
+
+
+// CChatApp 생성
+
+CChatApp::CChatApp()
+{
+	// TODO: 여기에 생성 코드를 추가합니다.
+	// InitInstance에 모든 중요한 초기화 작업을 배치합니다.
+}
+
+
+// 유일한 CChatApp 개체입니다.
+
+CChatApp theApp;
+
+
+// CChatApp 초기화
+
+BOOL CChatApp::InitInstance()
+{
+	CWinApp::InitInstance();
+
+	CChatDlg dlg;
+	m_pMainWnd = &dlg;
+	dlg.DoModal();
+	
+	return FALSE;
+}
+
