ac88d754bd94abe24d5d7fcd2b99c1a3e8bec347
jsk890 <resqme89@naver.com>
2022-04-27 18:21:31 +0900

    도형 그리기

---
+++ DrawShape/DrawShape.cpp
@@ -0,0 +1,50 @@
+﻿
+// DrawShape.cpp: 애플리케이션에 대한 클래스 동작을 정의합니다.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "DrawShape.h"
+#include "DrawShapeDlg.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CDrawShapeApp
+
+BEGIN_MESSAGE_MAP(CDrawShapeApp, CWinApp)
+	ON_COMMAND(ID_HELP, &CWinApp::OnHelp)
+END_MESSAGE_MAP()
+
+
+// CDrawShapeApp 생성
+
+CDrawShapeApp::CDrawShapeApp()
+{
+	// TODO: 여기에 생성 코드를 추가합니다.
+	// InitInstance에 모든 중요한 초기화 작업을 배치합니다.
+}
+
+
+// 유일한 CDrawShapeApp 개체입니다.
+
+CDrawShapeApp theApp;
+
+
+// CDrawShapeApp 초기화
+
+BOOL CDrawShapeApp::InitInstance()
+{
+
+	CWinApp::InitInstance();
+
+	CDrawShapeDlg dlg;
+	m_pMainWnd = &dlg;
+	dlg.DoModal();
+
+	return FALSE;
+
+}
+
